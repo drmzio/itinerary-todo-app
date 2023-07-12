6f679ec4bfc03213032a3b2e411fe32a5c410c71
Daniel Ramirez <6914518+drmzio@users.noreply.github.com>
2023-07-11 18:55:25 -0700

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      // ...
+    },
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/forms')],
+};
