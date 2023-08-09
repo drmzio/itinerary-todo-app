9b78a2c4c11e63c8b22eafffc286c4f979900045
Daniel Ramirez <6914518+drmzio@users.noreply.github.com>
2023-08-09 16:06:21 -0700

    Revert "Updates"
    This reverts commit 6d52ae2083ade59269cd62676dfc0fee433af6e0.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,9 @@
 import konstaConfig from 'konsta/config';
-import colors from 'tailwindcss/colors';
 
 /** @type {import('tailwindcss').Config} */
 export default konstaConfig({
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'media',
-  theme: {
-    extend: {
-      colors: {
-        gray: colors.neutral,
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 });
